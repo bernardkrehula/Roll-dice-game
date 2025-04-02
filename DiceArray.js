d3fb4eea5bd7f0cfac44019ce871a1a39239b865
bernardkrehula <krehula.bernard456@gmail.com>
2025-04-02 17:26:00 +0200

    get random dice via id

---
+++ DiceArray.js
@@ -0,0 +1,26 @@
+export const dices = [
+    {
+        id: 1,
+        img: "img/Dice-1--Streamline-Lucide.png",
+    },
+    {
+        id: 2,
+        img: "img/Dice-2--Streamline-Tabler.png",
+    },
+    {
+        id: 3,
+        img: "img/Dice-3--Streamline-Lucide.png",
+    },
+    {
+        id: 4,
+        img: "img/Dice-4--Streamline-Lucide.png",
+    },
+    {
+        id: 5,
+        img: "img/Dice-5--Streamline-Lucide.png",
+    },
+    {
+        id: 6,
+        img: "img/Dice-6--Streamline-Lucide.png",
+    },
+];
--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-//2 varijable playerOneCurrentScore, playerTwoCurrentScore
-//2 varijable playerOneTotalScore, playerTwoTotalScore
+//varijabla playerCurrentScore
+//varijabla playerTotalScore
 //Funkcija playerScoreCreator
 //Funkcija scoreManager
 //Funkcija Roll dice
 //Funkcija hold
+import { dices } from "./DiceArray.js";
+const newGame = document.querySelector('.new-game');
+const roll = document.querySelector('.roll-dice');
+const hold = document.querySelector('.hold');
+
+function playerScoreCreator() {
+    let playerCurrentScore;
+    let playerTotalScore;
+
+    const getCurrentSocre = () => {
+        return playerCurrentScore;
+    }   
+    const getTotalScore = () => {
+        return playerTotalScore;
+    }
+    return { getCurrentSocre, getTotalScore }
+}
+function scoreManager(){
+    const playerScoresArray  = [];
+
+    const pushScoreInArray = () => {
+        playerScoreCreator.push();
+    }
+    const giveRandomDice = () => {
+       let find = Math.floor(Math.random() * 6);
+       return dices.find(dice => dice.id == find);
+    }
+
+    return { giveRandomDice, pushScoreInArray }
+}
+const manager = scoreManager();
+
+roll.addEventListener('click', () => {
+    console.log('radi')
+});
+console.log(manager.giveRandomDice())
