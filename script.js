39fc01fcd8657a4324f64e9186a8df42f60e49d0
bernardkrehula <krehula.bernard456@gmail.com>
2025-04-06 18:06:09 +0200

    Code optimized for four players

--- script.js
+++ script.js
@@ -18,11 +18,17 @@ const hold = document.querySelector('.hold');
 const randomDice = document.querySelector('.all-btns img');
 
 const playerOneScore = document.querySelector(".total-scoreOne");
-const playerTwoScore = document.querySelector(".total-scoreTwo"); 
+const playerTwoScore = document.querySelector(".total-scoreTwo");
+const playerThreeScore = document.querySelector(".total-scoreThree");
+const playerFourScore = document.querySelector(".total-scoreFour");
+
 const playerOneCurrent = document.querySelector(".player-one .currentScore h2"); 
 const playerTwoCurrent = document.querySelector(".player-two .currentScore h2");
+const playerThreeCurrent = document.querySelector(".player-three .currentScore h2");
+const playerFourCurrent = document.querySelector(".player-four .currentScore h2");
 
 function playerScoreCreator() {
+    let serialNumber = 0;
     let playerCurrentScore = 0;
     let playerTotalScore = 0;
     let active = false; 
@@ -37,11 +43,15 @@ function playerScoreCreator() {
 
     const isActive = () => active;
     const toggleActive = (value) => { active = value };
+    const givePlayerSerialNumber = (value) => { serialNumber = value };
+    const playerSerialNumber = () => { return serialNumber };
 
-    return { getCurrentScore, setCurrentScore, addToCurrentScore, getTotalScore, addToTotalScore, resetTotalScore, isActive, toggleActive };
+    return { getCurrentScore, setCurrentScore, addToCurrentScore, getTotalScore, addToTotalScore, resetTotalScore, isActive, toggleActive, playerSerialNumber, givePlayerSerialNumber };
 }
 
 function scoreManager() {
+    let orderedPlayer = 0;
+
     const playerScoresArray = [];
 
     const pushScoreInArray = (player) => playerScoresArray.push(player);
@@ -50,57 +60,53 @@ function scoreManager() {
 
     const giveRandomDice = (id) => dices.find(dice => dice.id === id).img;
 
-    const checkActivePlayer = () => playerScoresArray.find(player => player.isActive());
+    const checkActivePlayer = () => playerScoresArray.find(player => player.playerSerialNumber() == orderedPlayer);
     
     const addValueToCurrentScore = (id) => {
         const selectedPlayer = checkActivePlayer();
         selectedPlayer.addToCurrentScore(id);
+        winner();
     };
 
     const resetOnDiceStatusOne = (randomId) => {
        if(randomId === 1){
             const selectedPlayer = checkActivePlayer();
             selectedPlayer.setCurrentScore(0);
-            changePlayerState();
+            setPlayerByOrder();
        }
     };
-
-    const setInitialState = () => {
-        playerScoresArray[0].toggleActive(true); 
-    };
-    
-    const changePlayerState = () => {
-        const currentPlayer = checkActivePlayer();
-        const nextPlayer = playerScoresArray.find(player => !player.isActive());
-        
-        currentPlayer.toggleActive(false);
-        nextPlayer.toggleActive(true); 
-        updateUI();
-    };
-    
+    const setPlayerByOrder = () => {
+        orderedPlayer = (orderedPlayer + 1) % playerScoresArray.length;
+        return orderedPlayer;
+    }
+    const winner = () => {
+        const selectedPlayer = checkActivePlayer();
+        if(selectedPlayer.getTotalScore() > 20 || selectedPlayer.getCurrentScore() > 20){
+            resetGame();
+        }
+    }
     const resetGame = () => {
         playerScoresArray.forEach(player => {
-            player.toggleActive(false);
             player.resetTotalScore();
             player.setCurrentScore(0);
         });
-        setInitialState();
+        orderedPlayer = 0;
         updateUI();
     };
 
     const returnArray = () => { return playerScoresArray };
 
-    return { giveRandomId, giveRandomDice, pushScoreInArray, checkActivePlayer, setInitialState, changePlayerState, addValueToCurrentScore, resetGame, returnArray, resetOnDiceStatusOne };
+    return { winner, setPlayerByOrder, giveRandomId, giveRandomDice, pushScoreInArray, checkActivePlayer, addValueToCurrentScore, resetGame, returnArray, resetOnDiceStatusOne };
 }
 
 const manager = scoreManager();
 randomDice.style.display = 'none';
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 4; i++) {
     const player = playerScoreCreator();
+    player.givePlayerSerialNumber(i);
     manager.pushScoreInArray(player);
 }
-manager.setInitialState(); 
 
 function displayRandomDice() {
     const diceId = manager.giveRandomId();
@@ -114,14 +120,18 @@ function displayRandomDice() {
 function updateUI() {
     const playerOne = manager.returnArray()[0];
     const playerTwo = manager.returnArray()[1];
+    const playerThree = manager.returnArray()[2];
+    const playerFour = manager.returnArray()[3];
     
     playerOneScore.textContent = playerOne.getTotalScore();
     playerTwoScore.textContent = playerTwo.getTotalScore();
+    playerThreeScore.textContent = playerThree.getTotalScore();
+    playerFourScore.textContent = playerFour.getTotalScore();
+
     playerOneCurrent.textContent = playerOne.getCurrentScore();
     playerTwoCurrent.textContent = playerTwo.getCurrentScore();
-    
-    document.querySelector(".player-one").classList.toggle("active", playerOne.isActive());
-    document.querySelector(".player-two").classList.toggle("active", playerTwo.isActive());
+    playerThreeCurrent.textContent = playerThree.getCurrentScore();
+    playerFourCurrent.textContent = playerFour.getCurrentScore();
 }
 
 roll.addEventListener('click', () => {
@@ -132,7 +142,7 @@ hold.addEventListener('click', () => {
     const currentPlayer = manager.checkActivePlayer();
     currentPlayer.addToTotalScore(currentPlayer.getCurrentScore());
     currentPlayer.setCurrentScore(0);
-    manager.changePlayerState();
+    manager.winner();
     updateUI();
 });
 
